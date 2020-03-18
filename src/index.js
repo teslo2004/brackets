81a24a6e8d1e4be7a9a76d389ce9b250669afa47
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2020-03-19 01:06:19 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+while (str.length > 0) {
+    var result = str.length;
+    str = str.replace(/\(\)/g, "");
+    str = str.replace(/\{\}/g, "");
+    str = str.replace(/\|\|/g, "");
+    str = str.replace(/\[\]/g, "");
+    str = str.replace(/12/g, "");
+    str = str.replace(/34/g, "");
+    str = str.replace(/56/g, "");
+    str = str.replace(/77/g, "");
+    str = str.replace(/88/g, "");
+    if (str.length === result) 
+      {
+        return false;
+      }
+    }
+    return true;
 }
